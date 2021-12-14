200b08bdd01072b08db50119e61c2d549c8130e3
unknown <you@example.com>
2021-12-14 15:32:59 +0700

    Úp bài

--- src/ss10_dsa/thuc_hanh/thuc_hanh_2/MyLinkedList.java
+++ src/ss10_dsa/thuc_hanh/thuc_hanh_2/MyLinkedList.java
@@ -1,4 +1,5 @@
-package ss10_dsa.thuc_hanh;
+package ss10_dsa.thuc_hanh.thuc_hanh_2;
+
 public class MyLinkedList {
     private Node head;
     private int numNodes;
@@ -7,9 +8,45 @@ public class MyLinkedList {
         head = new Node(data);
     }
 
+    public void add(int index, Object data) {
+        Node temp = head;
+        Node holder;
+
+        for (int i = 0; i < index - 1 && temp.next != null; i++) {
+            temp = temp.next;
+        }
+        holder = temp.next;
+        temp.next = new Node(data);
+        temp.next.next = holder;
+        numNodes++;
+    }
+
+    public void addFirst(Object data) {
+        Node temp = head;
+        head = new Node(data);
+        head.next = temp;
+        numNodes++;
+    }
+
+    public Node get(int index) {
+        Node temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
+
     private class Node {
         private Node next;
-        private Object data;
+        private final Object data;
 
         public Node(Object data) {
             this.data = data;
@@ -18,39 +55,5 @@ public class MyLinkedList {
         public Object getData() {
             return this.data;
         }
-
-        public void add(int index, Object data) {
-            Node temp = head;
-            Node holder;
-
-            for (int i = 0; i < index - 1 && temp.next != null; i++) {
-                temp = temp.next;
-            }
-            holder = temp.next;
-            temp.next = new Node(data);
-            temp.next.next = holder;
-            numNodes++;
-        }
-
-        public void addFirst(Object data) {
-            Node temp = head;
-            head = new Node(data);
-            head.next = temp;
-            numNodes++;
-        }
-
-        public Node get(int index) {
-            Node temp = head;
-            for (int i = 0; i < index; i++) {
-                temp = temp.next;
-            }
-            return temp;
-        }
-        public void printList() {
-            Node temp = head;
-            while(temp != null) {
-                System.out.println(temp.data);
-                temp = temp.next;
-            }
     }
 }
