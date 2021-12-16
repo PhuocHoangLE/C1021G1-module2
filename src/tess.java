fbc3168961e7629fdbc96228f5a7ec0ea7d43cd6
unknown <you@example.com>
2021-12-16 17:12:04 +0700

    Úp bài

--- src/tess.java
+++ src/tess.java
@@ -1,22 +1,14 @@
-public class tess {
-    static void total(String name , int age){
-        System.out.println(name+ " I just got execute " + age);
-    }
-    static int nums(int x){
-        return 5 + x ;
-    }
-    static int c(int q,int w){
-        return q + w ;
-    }
-    static double g(double q , double w){
-        return q + w ;
-    }
-    public static void main(String[] args) {
-        total("Hoang" , 5);
-        System.out.println(nums(10)) ;
-        int total1 = c(8,5) ;
-        double total2 = g(3.2,5.4) ;
-        System.out.println("Int : " +total1);
-        System.out.println("Double :"+total2);
+import java.util.LinkedList;
+
+    public class tess {
+        public static void main(String[] args) {
+            LinkedList list = new LinkedList();
+            list.add(3);
+            list.add(1);
+            list.add(2);
+            list.add(4);
+            System.out.println(list.getFirst());
         }
-}
+    }
+
+
