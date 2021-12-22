d86214f4735fdb14a8873026787c2ad4161d4c98
unknown <you@example.com>
2021-12-22 15:35:03 +0700

    upbai

---
+++ src/ss15_xu_ly_ngoai_le_debug/bai_tap/IllegalTriangleException.java
@@ -0,0 +1,12 @@
+package ss15_xu_ly_ngoai_le_debug.bai_tap;
+
+public class IllegalTriangleException extends Exception{
+
+    public IllegalTriangleException() {
+        super();
+    }
+
+    public IllegalTriangleException(String message) {
+        super(message);
+    }
+}
